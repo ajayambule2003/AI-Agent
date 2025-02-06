340244b5ea9d0a5f39a34e512d07faf191e2c217
Ajay Ambule <ajayambule2003@gmail.com>
2025-02-06 11:07:22 +0530

    Updated version

---
+++ memory.py
@@ -0,0 +1,12 @@
+class Memory:
+    def __init__(self):
+        self.history = []
+
+    def add_interaction(self, user_input, response):
+        self.history.append((user_input, response))
+
+    def get_history(self):
+        return self.history
+
+    def clear_memory(self):
+        self.history = []

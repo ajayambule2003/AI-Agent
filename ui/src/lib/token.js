b9cfa1a290f511ef655054e99191289835810e60
Ajay Ambule <ajayambule2003@gmail.com>
2025-05-11 01:04:36 +0530

    token.js file updated:

--- ui/src/lib/token.js
+++ ui/src/lib/token.js
@@ -1,11 +1,12 @@
-import { Tiktoken } from "tiktoken/lite";
 import cl100k_base from "tiktoken/encoders/cl100k_base.json";
+import { Tiktoken } from "tiktoken/lite";
 
 const encoding = new Tiktoken(
   cl100k_base.bpe_ranks,
   cl100k_base.special_tokens,
   cl100k_base.pat_str
 );
+
 
 export function calculateTokens(text) {
   const tokens = encoding.encode(text);

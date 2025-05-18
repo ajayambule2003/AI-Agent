240175af2437f5a156e95f38ba7ab85eead02497
Ajay Ambule <ajayambule2003@gmail.com>
2025-05-18 11:13:11 +0530

    api file Updated:

--- ui/src/lib/api.js
+++ ui/src/lib/api.js
@@ -20,9 +20,7 @@ const getApiBaseUrl = () => {
   } else {
     return 'http://127.0.0.1:1337';
   }
-};
-
-
+}; 
 export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || getApiBaseUrl();
 export const socket = io(API_BASE_URL, { autoConnect: false });
 

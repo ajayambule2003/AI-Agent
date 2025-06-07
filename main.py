4cef8d058092ddd61885aa90b504d09b81928305
Ajay Ambule <129256812+ajayambule2003@users.noreply.github.com>
2025-06-07 13:11:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def main():
     print("Simple AI Agent: Hi! Type something to talk to me. Type 'bye' to exit.") 
 
 # Here is function
-
+ 
     while True: 
         user_input = input("You: ")
         if user_input.lower() == "bye":

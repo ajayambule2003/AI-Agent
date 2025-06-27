2f9dc3f32b3f013d1f5b70010d5ed44c7392f697
Ajay Ambule <ajayambule2003@gmail.com>
2025-06-27 22:41:08 +0530

    Updated File:

--- main.py
+++ main.py
@@ -2,16 +2,15 @@ from agent import SimpleAIAgent
 
 def main():
     agent = SimpleAIAgent()
-    print("Simple AI Agent: Hi! Type something to talk to me. Type 'bye' to exit.") 
+    print("Simple AI Agent: Hi! Type something to talk to me. Type 'bye' to exit.")
 
-# Here is function
-
-    while True: 
+    while True:
         user_input = input("You: ")
         if user_input.lower() == "bye":
             print("Simple AI Agent:", agent.respond(user_input))
-            break 
+            break
         else:
             print("Simple AI Agent:", agent.respond(user_input))
+
 if __name__ == "__main__":
-    main()    
+    main()

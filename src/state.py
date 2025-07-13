a7f40277fde1522bfb27047754a67f95d2a43dfd
Ajay Ambule <129256812+ajayambule2003@users.noreply.github.com>
2025-07-13 10:22:08 +0530

    Update state.py

--- src/state.py
+++ src/state.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from typing import Optional
 from sqlmodel import Field, Session, SQLModel, create_engine
 from src.socket_instance import emit_agent
-from src.config import Config
+from src.config import Config 
 
 class AgentStateModel(SQLModel, table=True):
     __tablename__ = "agent_state"

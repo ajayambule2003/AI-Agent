032f02349abcbbe38bff980e813747854e075462
Ajay Ambule <129256812+ajayambule2003@users.noreply.github.com>
2025-08-20 14:05:21 +0530

    Update devika.py

--- devika.py
+++ devika.py
@@ -9,7 +9,7 @@ aetherAI()
  
  
 from flask import Flask, request, jsonify, send_file  
-from flask_cors import CORS
+from flask_cors import CORS 
 from src.socket_instance import socketio, emit_agent
 import os
 import logging

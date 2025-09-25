8b7e8a14fd5d54f8f62842c9c90f65834f47dde4
Ajay Ambule <129256812+ajayambule2003@users.noreply.github.com>
2025-09-25 08:52:34 +0530

    Update project.py

--- src/project.py
+++ src/project.py
@@ -7,7 +7,6 @@ from src.socket_instance import emit_agent
 from sqlmodel import Field, Session, SQLModel, create_engine
 from src.config import Config 
  
-
 class Projects(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)  
     project: str

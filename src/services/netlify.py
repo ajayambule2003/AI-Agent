ef01aa4060098c0f4122745060586c2580c18602
Ajay Ambule <129256812+ajayambule2003@users.noreply.github.com>
2026-08-20 19:55:54 +0530

    Update netlify.py

--- src/services/netlify.py
+++ src/services/netlify.py
@@ -8,7 +8,7 @@ class Netlify:
         config = Config()
         api_key = config.get_netlify_api_key()
         self.netlify = NetlifyPy(access_token=api_key)  
-
+ 
     def deploy(self, project_name: str): 
         project_path = ProjectManager().get_project_path(project_name)
         
